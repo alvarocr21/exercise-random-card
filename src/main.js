2d89c4ed19333ba6b957d2b5cb5884cbe4cbd69e
alvarocr21 <alvarocr21@gmail.com>
2021-02-16 03:20:10 +0000

    Primer commit

--- src/main.js
+++ src/main.js
@@ -6,5 +6,16 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  console.log("Hello Rigo from the console!");
+  let carta = document.getElementById("carta");
+  let numCard = document.getElementById("numero");
+  let numeros = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+  let palos = ["diamond", "hearts", "clubs", "spades"];
+
+  let numAleatorio = Math.floor(Math.random() * 13);
+  let num = numeros[numAleatorio];
+  let palAleatorio = Math.floor(Math.random() * 4);
+  let pal = palos[palAleatorio];
+
+  carta.classList.add("carta", pal);
+  numCard.innerHTML = "<div class='numero'>" + num + "</div>";
 };
